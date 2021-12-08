190a40931cd0a9564ec22c172c5ea28f44a9fb2f
Emma <emma.mccracken14@gmail.com>
2021-12-08 15:41:04 +0000

    add endpoints for comments

--- server.ts
+++ server.ts
@@ -27,7 +27,7 @@ const client = new Client(dbConfig);
 client.connect();
 
 // GET 10 most recent pastes
-app.get("/", async (req, res) => {
+app.get("/pastes", async (req, res) => {
   const dbres = await client.query(
     "SELECT * FROM pastes ORDER BY date DESC LIMIT 10"
   );
@@ -35,7 +35,7 @@ app.get("/", async (req, res) => {
 });
 
 // GET specific paste by id
-app.get("/:n", async (req, res) => {
+app.get("/pastes/:n", async (req, res) => {
   const n = req.params.n;
   const dbres = await client.query("SELECT * FROM pastes WHERE id = $1", [n]);
   const result = dbres.rows;
@@ -52,7 +52,7 @@ app.get("/:n", async (req, res) => {
 });
 
 // GET n most recent pastes
-app.get("/pastes/:n", async (req, res) => {
+app.get("/pastes/limit/:n", async (req, res) => {
   const n = req.params.n;
   const dbres = await client.query(
     "SELECT * FROM pastes ORDER BY date DESC LIMIT $1",
@@ -61,7 +61,7 @@ app.get("/pastes/:n", async (req, res) => {
   res.json(dbres.rows);
 });
 
-app.post("/", async (req, res) => {
+app.post("/pastes", async (req, res) => {
   const { title, text } = req.body;
 
   if (
@@ -87,7 +87,7 @@ app.post("/", async (req, res) => {
 });
 
 // DELETE specific paste by id
-app.delete("/:id", async (req, res) => {
+app.delete("/pastes/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   const dbres = await client.query(
     "DELETE FROM pastes WHERE id = $1 returning *",
@@ -108,7 +108,7 @@ app.delete("/:id", async (req, res) => {
 });
 
 // Update specific paste by id
-app.put("/:id", async (req, res) => {
+app.put("/pastes/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   const { title, text } = req.body;
   let result = [];
@@ -148,6 +148,152 @@ app.put("/:id", async (req, res) => {
     res.status(404).json({
       status:
         "failed: id does not exist, title should not exceed 50 characters, body should contain atleast a title or text",
+      data: id,
+    });
+  }
+});
+
+// GET 10 most recent comments
+app.get("/comments", async (req, res) => {
+  const dbres = await client.query(
+    "SELECT * FROM comments ORDER BY date DESC LIMIT 10"
+  );
+  res.json(dbres.rows);
+});
+
+// GET specific comment by comment_id
+app.get("/comments/:n", async (req, res) => {
+  const n = req.params.n;
+  const dbres = await client.query(
+    "SELECT * FROM comments WHERE comment_id = $1",
+    [n]
+  );
+  const result = dbres.rows;
+  if (result.length === 1) {
+    res.status(200).json({
+      status: "success",
+      data: result,
+    });
+  } else {
+    res.status(404).json({
+      status: "failed: id does not exist",
+    });
+  }
+});
+
+// GET n most recent comments
+app.get("/comments/limit/:n", async (req, res) => {
+  const n = req.params.n;
+  const dbres = await client.query(
+    "SELECT * FROM comments ORDER BY date DESC LIMIT $1",
+    [n]
+  );
+  res.json(dbres.rows);
+});
+
+//GET comments on paste with id n
+app.get("/paste/:id/comments", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const dbres = await client.query(
+      "SELECT * FROM comments WHERE paste_id = $1 ORDER BY date DESC LIMIT 10 ",
+      [id]
+    );
+    const result = dbres.rows;
+
+    res.status(200).json({
+      status: "success",
+      data: result,
+    });
+  } catch {
+    res.status(404).json({
+      status: "failed: id does not exist",
+    });
+  }
+});
+
+app.post("/comments/:paste_id", async (req, res) => {
+  const { text } = req.body;
+  const paste_id = parseInt(req.params.paste_id);
+  if (typeof text === "string" && text !== "") {
+    try {
+      const dbres = await client.query(
+        "INSERT INTO comments (paste_id, text) VALUES ($1, $2) returning *",
+        [paste_id, text]
+      );
+
+      res.status(200).json({
+        status: "success",
+        data: dbres.rows,
+      });
+    } catch {
+      res.status(404).json({
+        status: "failed: paste_id does not exist",
+        data: req.body,
+      });
+    }
+  } else {
+    res.status(404).json({
+      status: "failed: text expects a non-empty string",
+      data: req.body,
+    });
+  }
+});
+
+// DELETE specific paste by id
+app.delete("/comments/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const dbres = await client.query(
+    "DELETE FROM comments WHERE comment_id = $1 returning *",
+    [id]
+  );
+  const result = dbres.rows;
+  if (result.length === 1) {
+    res.status(200).json({
+      status: "success",
+      data: result,
+    });
+  } else {
+    res.status(404).json({
+      status: "failed: comment_id does not exist",
+      data: id,
+    });
+  }
+});
+
+// Update specific comment by id
+app.put("/comments/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const { text } = req.body;
+  let result = [];
+  if (typeof text === "string" && text !== "") {
+    try {
+      const dbres = await client.query(
+        "UPDATE comments SET  text = $1 WHERE comment_id = $2 returning *",
+        [text, id]
+      );
+      result = dbres.rows;
+      if (dbres.rows.length === 1) {
+        res.status(200).json({
+          status: "success",
+          data: result,
+        });
+      } else {
+        res.status(404).json({
+          status: "failed: id does not exist",
+          data: { id: id, text: text },
+        });
+      }
+    } catch (err) {
+      res.status(404).json({
+        status: "failed",
+        data: { id: id, text: text },
+        error: err,
+      });
+    }
+  } else {
+    res.status(404).json({
+      status: "failed: Body should contain a non-empty string for text",
       data: id,
     });
   }
